a60fcb698c0b723b42d8ab7c2d78acb52d76716e
Yamaguchi Shunsuke <null.pointer.exception.line404@email.com>
2026-01-20 13:50:27 +0900

    rename header title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Todo List </q-toolbar-title>
       </q-toolbar>
     </q-header>
     <q-page-container>
