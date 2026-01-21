254347ad90014bd76a1ebf3568c840479d3855fe
Yamaguchi Shunsuke <null.pointer.exception.line404@email.com>
2026-01-21 11:19:53 +0900

    fix lib version and be able to run hello for firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,9 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+import { setGlobalOptions } from 'firebase-functions';
+import { onRequest } from 'firebase-functions/https';
+import * as logger from 'firebase-functions/logger';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -26,7 +26,7 @@ import * as logger from "firebase-functions/logger";
 // this will be the maximum concurrent request count.
 setGlobalOptions({ maxInstances: 10 });
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+  logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
