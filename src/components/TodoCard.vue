686d3a9afd69da3336d0fd486693add58d1ad7ba
Yamaguchi Shunsuke <null.pointer.exception.line404@email.com>
2026-01-21 13:01:03 +0900

    feat: done

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,5 +1,9 @@
 <template>
   <q-card>
+    <q-checkbox
+      :model-value="todo.done"
+      @update:model-value="emit('toggle-done', todo.id, $event)"
+    ></q-checkbox>
     {{ todo.content }}
   </q-card>
   <q-btn v-on:click="emit('remove', todo.id)">x</q-btn>
@@ -14,6 +18,7 @@ interface Props {
 
 interface Emits {
   (e: 'remove', todoId: number): void;
+  (e: 'toggle-done', id: number, next: boolean): void;
 }
 
 const { todo } = defineProps<Props>();
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
   <q-list>
     <q-item v-for="todo in props.todos" :key="todo.id">
-      <todo-card :todo="todo" @remove="emit('remove', $event)"> </todo-card>
+      <todo-card
+        :todo="todo"
+        @remove="emit('remove', $event)"
+        @toggle-done="(id, next) => emit('toggle-done', id, next)"
+      >
+      </todo-card>
     </q-item>
   </q-list>
 </template>
@@ -12,6 +17,7 @@ import TodoCard from './TodoCard.vue';
 
 interface Emits {
   (e: 'remove', id: number): void;
+  (e: 'toggle-done', id: number, next: boolean): void;
 }
 
 interface Props {
